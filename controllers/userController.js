cbd5784c007b6a7c6a6991f20981a9e01f3f62b6
N Z <nelectrik@gmail.com>
2023-02-05 21:58:14 +1030

    Last commit

--- controllers/userController.js
+++ controllers/userController.js
@@ -19,7 +19,7 @@ module.exports = {
       .select("-__v")
       .populate("friends")
       .populate("thoughts")
-      .then(async (user) =>
+      .then((user) =>
         !user
           ? res.status(404).json({ message: "No user with that ID" })
           : res.json(user)
